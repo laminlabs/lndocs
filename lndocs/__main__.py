f0192499cc73811b12f27b61dc6129f7eda1336a
Alex Wolf <f.alexander.wolf@gmail.com>
2022-05-13 16:42:19 +0200

    :building_construction: move it into package

--- lndocs/__main__.py
+++ lndocs/__main__.py
@@ -13,5 +13,5 @@ def main():
     aa("--docs", type=str, default="docs", help="directory with docs sources")
     aa("--site", type=str, default="_build/html", help="output directory")
     args = parser.parse_args()
-    sync(str(HERE.parent / "lamin_sphinx"), "./lamin_sphinx", "sync", create=True)
+    sync(str(HERE / "lamin_sphinx"), "./lamin_sphinx", "sync", create=True)
     os.system(f"sphinx-build {args.docs} {args.site}")
