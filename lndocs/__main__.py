b804f763275d121181736c71eaefa6f04ee7046d
Alex Wolf <f.alexander.wolf@gmail.com>
2022-05-13 13:27:20 +0200

    :ambulance: fix output dir

--- lndocs/__main__.py
+++ lndocs/__main__.py
@@ -6,6 +6,6 @@ def main():
     parser = argparse.ArgumentParser(description="Build Lamin website.")
     aa = parser.add_argument
     aa("--docs", type=str, default="docs", help="directory with docs sources")
-    aa("--site", type=str, default="_build", help="output directory")
+    aa("--site", type=str, default="_build/html", help="output directory")
     args = parser.parse_args()
     os.system(f"sphinx-build {args.docs} {args.site}")
