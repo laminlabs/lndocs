9e9b16e24646f1cb2d374f68b41560d7a41f1959
Alex Wolf <f.alexander.wolf@gmail.com>
2022-08-02 01:05:25 +0200

    :white_check_mark: Add session

--- lndocs/lamin_sphinx/__init__.py
+++ lndocs/lamin_sphinx/__init__.py
@@ -96,6 +96,7 @@ nitpicky = True  # report broken links
 
 nitpick_ignore = [
     ("py:class", "pandas.core.frame.DataFrame"),
+    ("py:class", "sqlmodel.orm.session.Session"),
     ("py:class", "numpy.ndarray"),
     ("py:class", "datetime.datetime"),
     ("py:class", "pydantic.main.BaseModel"),
