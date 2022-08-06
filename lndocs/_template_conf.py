fcdb5f5c2042fd2b1753cd551e983b38e03a2a59
Alex Wolf <f.alexander.wolf@gmail.com>
2022-08-06 23:17:03 +0200

    :memo: Also fix for docs

--- lndocs/_template_conf.py
+++ lndocs/_template_conf.py
@@ -14,6 +14,6 @@ html_title = "{{ lamin_project_name }} | Lamin Labs"
 release = {{ lamin_package_name }}.__version__
 html_context["github_repo"] = "{{ lamin_repository_name }}"  # noqa
 # We're actually using this for the link behind the brand of the page!
-html_theme_options["logo"] = {"link": "{{ lamin_project_slug }}"}  # noqa
+html_theme_options["logo"] = {"link": "docs/{{ lamin_project_slug }}"}  # noqa
 
 ogp_site_url = "https://lamin.ai/docs/{{ lamin_project_slug }}"
