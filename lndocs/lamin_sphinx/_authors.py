f581eb094337f3bc49af6274aa3f824544e39d26
Alex Wolf <f.alexander.wolf@gmail.com>
2022-10-21 16:43:18 +0200

    :bento: Add Fred & Bernardo as authors

--- lndocs/lamin_sphinx/_authors.py
+++ lndocs/lamin_sphinx/_authors.py
@@ -1,6 +1,8 @@
 authors = {
+    "bpenteado": ("Bernardo Penteado", "https://pbern.com"),
     "falexwolf": ("Alex Wolf", "https://falexwolf.me"),
-    "sunnyosun": ("Sunny Sun", "https://github.com/sunnyosun"),
+    "fredericenard": ("Frederic Enard", "https://github.com/fredericenard"),
     "Koncopd": ("Sergei Rybakov", "https://github.com/Koncopd"),
+    "sunnyosun": ("Sunny Sun", "https://github.com/sunnyosun"),
     "Zethson": ("Lukas Heumos", "https://github.com/Zethson"),
 }
