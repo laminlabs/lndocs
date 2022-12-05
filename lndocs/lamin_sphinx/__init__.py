cecc48a1e5fd4e2b8ba53a19f3e2773f00384fdc
Alex Wolf <f.alexander.wolf@gmail.com>
2022-12-06 00:27:57 +0100

    :sparkles: Add wetlab schema to intersphinx

--- lndocs/lamin_sphinx/__init__.py
+++ lndocs/lamin_sphinx/__init__.py
@@ -96,6 +96,7 @@ intersphinx_mapping = dict(
     docs=("https://lamin.ai/docs", None),
     lnschema_core=("https://lamin.ai/docs/lnschema-core", None),
     lnschema_bionty=("https://lamin.ai/docs/lnschema-bionty", None),
+    lnschema_wetlab=("https://lamin.ai/docs/lnschema-wetlab", None),
     bionty=("https://lamin.ai/docs/bionty", None),
 )
 
