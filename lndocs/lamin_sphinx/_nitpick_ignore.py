bf2d9c19e18de6a970709689de8b59632e56dae5
Alex Wolf <f.alexander.wolf@gmail.com>
2022-12-06 00:33:24 +0100

    :green_heart: Ignore BiosampleBase for now

--- lndocs/lamin_sphinx/_nitpick_ignore.py
+++ lndocs/lamin_sphinx/_nitpick_ignore.py
@@ -6,6 +6,8 @@ nitpick_ignore = [
     ("py:class", "lnschema_core._core.jupynb"),
     ("py:class", "lnschema_wetlab._core.Biosample"),
     ("py:class", "lnschema_wetlab._core.Techsample"),
+    ("py:class", "lnschema_wetlab._core.BiosampleBase"),
+    ("py:class", "lnschema_wetlab._core.TechsampleBase"),
     ("py:class", "bionty._table.EntityTable"),
     ("py:class", "bionty._species._core.Species"),
     ("py:class", "bionty._cellmarker._core.CellMarker"),
