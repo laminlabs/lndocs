e797add45f0c6e3531ecaa68c18227574c3f79bd
Alex Wolf <f.alexander.wolf@gmail.com>
2022-12-16 08:56:33 +0100

    :bug: Fix

--- lndocs/lamin_sphinx/_nitpick_ignore.py
+++ lndocs/lamin_sphinx/_nitpick_ignore.py
@@ -1,5 +1,5 @@
 nitpick_ignore = [
-    ("py:class", "cloudpathlib.s3.gsclient.GSClient"),
+    ("py:class", "cloudpathlib.gs.gsclient.GSClient"),
     ("py:class", "cloudpathlib.s3.s3client.S3Client"),
     ("py:class", "lnschema_core.dev.sqlmodel.SQLModelModule"),
     ("py:class", "lnschema_core.dev.sqlmodel.SQLModelPrefix"),
