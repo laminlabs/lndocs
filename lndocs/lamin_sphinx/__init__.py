fdfbd52f1913643913a814d8a378707cc80ebf62
Alex Wolf <f.alexander.wolf@gmail.com>
2023-02-17 19:42:35 +0100

    :wrench: Add lndb to intersphinx

--- lndocs/lamin_sphinx/__init__.py
+++ lndocs/lamin_sphinx/__init__.py
@@ -99,6 +99,7 @@ ogp_image = (
 
 intersphinx_mapping = dict(
     docs=("https://lamin.ai/docs", None),
+    lndb=("https://lamin.ai/docs/lndb", None),
     lnschema_core=("https://lamin.ai/docs/lnschema-core", None),
     lnschema_bionty=("https://lamin.ai/docs/lnschema-bionty", None),
     lnschema_wetlab=("https://lamin.ai/docs/lnschema-wetlab", None),
