352630da330d67b28ac7d9b422e7ff879cdec79e
Alex Wolf <f.alexander.wolf@gmail.com>
2023-03-08 15:57:39 +0100

    :wrench: Add search button to all pages

--- lndocs/lamin_sphinx/__init__.py
+++ lndocs/lamin_sphinx/__init__.py
@@ -44,7 +44,7 @@ html_theme_options = {
     "show_prev_next": True,
     "use_edit_page_button": False,
     # "search_bar_text": "Search",  # currently unused
-    # "navbar_persistent": "",  # hides the search/magnifier icon
+    "navbar_persistent": "search-button.html",
     "show_toc_level": 1,
     # "show_nav_level": 2,
     "navbar_end": ["theme-switcher", "navbar-icon-links"],
