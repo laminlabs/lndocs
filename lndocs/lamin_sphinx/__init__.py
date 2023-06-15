064d24d23d1577dbe3da1ddc61aa7c7d56153320
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-15 21:54:53 +0200

    💄 Prettify

--- lndocs/lamin_sphinx/__init__.py
+++ lndocs/lamin_sphinx/__init__.py
@@ -157,7 +157,10 @@ def process_docstring(app, what, name, obj, options, lines):
             ]
             for attr in attributes:
                 lines.append(f".. autoattribute:: {attr[0]}\n")
-                lines.append(f"   :annotation: :class:`~{type(attr[1])}`")
+                if isinstance(attr[1], property):
+                    lines.append(f"   :annotation: {type(attr[1]).__name__}")
+                else:
+                    lines.append(f"   :annotation: {type(attr[1]).__name__}")
         # the following is more complicated than expected, leave this in template for now  # noqa
         # lines.append(f".. rubric:: Methods")
         # methods = inspect.getmembers(obj, lambda a:not(inspect.isroutine(a) or inspect.isfunction(a)))  # noqa
