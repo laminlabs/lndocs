7adad4eb073f46da2148d58977ea42c40eb60b42
Alex Wolf <f.alexander.wolf@gmail.com>
2025-03-05 09:08:38 +0100

    💚 Try to fix docs for non-instance

--- lndocs/lamin_sphinx/__init__.py
+++ lndocs/lamin_sphinx/__init__.py
@@ -549,74 +549,73 @@ def attach_func_to_class_method(func_name, cls, globals):
 from typing import NamedTuple  # noqa
 
 import pandas as pd  # noqa
-from lamindb.base import doc_args  # noqa
-from lamindb.base.types import StrField  # noqa
-from lamindb.models import QuerySet, Record  # noqa
 
+try:
+    from lamindb.base import doc_args  # noqa
+    from lamindb.base.types import StrField  # noqa
+    from lamindb.models import QuerySet, Record  # noqa
 
-@classmethod  # type:ignore
-@doc_args(Record.filter.__doc__)
-def filter(cls, *queries, **expressions) -> QuerySet:
-    """{}"""  # noqa: D415
-    pass
+    @classmethod  # type:ignore
+    @doc_args(Record.filter.__doc__)
+    def filter(cls, *queries, **expressions) -> QuerySet:
+        """{}"""  # noqa: D415
+        pass
 
+    @classmethod  # type:ignore
+    @doc_args(Record.get.__doc__)
+    def get(
+        cls,
+        idlike: int | str | None = None,
+        **expressions,
+    ) -> Record:
+        """{}"""  # noqa: D415
+        pass
 
-@classmethod  # type:ignore
-@doc_args(Record.get.__doc__)
-def get(
-    cls,
-    idlike: int | str | None = None,
-    **expressions,
-) -> Record:
-    """{}"""  # noqa: D415
-    pass
+    @classmethod  # type:ignore
+    @doc_args(Record.df.__doc__)
+    def df(
+        cls,
+        include: str | list[str] | None = None,
+        features: bool | list[str] = False,
+        limit: int = 100,
+    ) -> pd.DataFrame:
+        """{}"""  # noqa: D415
+        pass
 
+    @classmethod  # type: ignore
+    @doc_args(Record.search.__doc__)
+    def search(
+        cls,
+        string: str,
+        *,
+        field: StrField | None = None,
+        limit: int | None = 20,
+        case_sensitive: bool = False,
+    ) -> QuerySet:
+        """{}"""  # noqa: D415
+        pass
 
-@classmethod  # type:ignore
-@doc_args(Record.df.__doc__)
-def df(
-    cls,
-    include: str | list[str] | None = None,
-    features: bool | list[str] = False,
-    limit: int = 100,
-) -> pd.DataFrame:
-    """{}"""  # noqa: D415
-    pass
+    @classmethod  # type: ignore
+    @doc_args(Record.lookup.__doc__)
+    def lookup(
+        cls,
+        field: StrField | None = None,
+        return_field: StrField | None = None,
+    ) -> NamedTuple:
+        """{}"""  # noqa: D415
+        pass
 
+    @classmethod  # type: ignore
+    @doc_args(Record.using.__doc__)
+    def using(
+        cls,
+        instance: str | None,
+    ) -> QuerySet:
+        """{}"""  # noqa: D415
+        pass
 
-@classmethod  # type: ignore
-@doc_args(Record.search.__doc__)
-def search(
-    cls,
-    string: str,
-    *,
-    field: StrField | None = None,
-    limit: int | None = 20,
-    case_sensitive: bool = False,
-) -> QuerySet:
-    """{}"""  # noqa: D415
-    pass
-
-
-@classmethod  # type: ignore
-@doc_args(Record.lookup.__doc__)
-def lookup(
-    cls,
-    field: StrField | None = None,
-    return_field: StrField | None = None,
-) -> NamedTuple:
-    """{}"""  # noqa: D415
-    pass
-
-
-@classmethod  # type: ignore
-@doc_args(Record.using.__doc__)
-def using(
-    cls,
-    instance: str | None,
-) -> QuerySet:
-    """{}"""  # noqa: D415
-    pass
+except Exception as err:
+    print("WARNING: DID NOT IMPORT LAMINDB", err)
 
 
 def get_all_annotations(obj):
