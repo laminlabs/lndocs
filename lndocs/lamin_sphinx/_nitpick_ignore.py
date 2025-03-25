d1aef2cbe1ad124153cbc895f73c17c53aba6bf6
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2025-03-25 12:36:30 +0100

    🙈 Update nitpick ignore

--- lndocs/lamin_sphinx/_nitpick_ignore.py
+++ lndocs/lamin_sphinx/_nitpick_ignore.py
@@ -81,6 +81,7 @@ nitpick_ignore = [
     ("py:class", "bionty_base._public_ontology.PublicOntology"),
     ("py:class", "bionty_base._public_ontology.PublicOntologyField"),
     ("py:class", "bionty.base.entities._gene.MappingResult"),
+    ("py:class", "bionty.base._ontology.Ontology"),
     ("py:class", "pronto.ontology.Ontology"),
     ("py:class", "pandas.core.frame.DataFrame"),
     ("py:class", "sqlmodel.orm.session.Session"),
